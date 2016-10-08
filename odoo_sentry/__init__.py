66f0d244e8481ab15bcaff75ddb584a371b33acc
Naglis Jonaitis <naglis@hbee.eu>
2016-10-08 19:07:53 +0300

    odoo_sentry: completely rewrites addon
    - rewrites addon based only on a custom sentry logger - no more WSGI
      application wrapping, etc.  This allows to simplify the addon and have both
      Odoo logs on the server and also Sentry reporting for eg. warning level and
      above.
    - extracts and adds additional context, if available, for example, Odoo
      database name is added as Sentry 'tags', user login and UID are added
      as Sentry 'user' context, current context is added as Sentry 'extra'
      context. Also, WSGI HTTP request is parsed and added to Sentry 'request'
      context. This allows to receive much more information inside Sentry.
    - Odoo git commit added as Sentry release to allow to differentiate between
      Odoo updates.
    
    WP#5338

--- odoo_sentry/__init__.py
+++ odoo_sentry/__init__.py
@@ -1,143 +1,60 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo - Sentry connector
-#    Copyright (C) 2014 Mohammed Barsi
-#    Copyright (C) 2015 Naglis Jonaitis
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# This file is part of Odoo. The COPYRIGHT file at the top level of
+# this module contains the full copyright notices and license terms.
 
-import logging
-import sys
-
-import openerp.service.wsgi_server
-import openerp.addons.web.controllers.main
-import openerp.addons.report.controllers.main
-import openerp.http
 import openerp.tools.config as config
-import openerp.osv.osv
-import openerp.exceptions
-import openerp.loglevels
-from openerp.http import request
-from raven import Client
-from raven.handlers.logging import SentryHandler
-from raven.middleware import Sentry
-from raven.conf import setup_logging, EXCLUDE_LOGGER_DEFAULTS
 
+from raven import Client, fetch_git_sha
+from raven.conf import setup_logging
+from raven.exceptions import InvalidGitRepository
 
-_logger = logging.getLogger(__name__)
-ORM_EXCEPTIONS = (
-    openerp.osv.osv.except_osv,
-    openerp.exceptions.Warning,
-    openerp.exceptions.AccessError,
-    openerp.exceptions.AccessDenied,
+from openerp.addons.odoo_sentry.logutils import (
+    DEFAULT_LOG_LEVEL,
+    LOG_LEVEL_MAP,
+    OdooSentryHandler,
+    UserErrorFilter,
 )
 
-LOGLEVELS = dict([
-    (getattr(openerp.loglevels, 'LOG_%s' % x), getattr(logging, x))
-    for x in ('CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET')
-])
 
-CLIENT_DSN = config.get('sentry_client_dsn', '').strip()
-ENABLE_LOGGING = config.get('sentry_enable_logging', False)
-ALLOW_ORM_WARNING = config.get('sentry_allow_orm_warning', False)
-INCLUDE_USER_CONTEXT = config.get('sentry_include_context', False)
-LOGGING_LEVEL = config.get('sentry_logging_level', 'warn')
-
-
-def get_user_context():
-    """Get the current user context, if possible."""
-    cxt = {}
+def get_odoo_commit(odoo_dir):
+    '''Attempts to get Odoo git commit from :param:`odoo_dir`.'''
+    if not odoo_dir:
+        return
     try:
-        session = getattr(request, 'session', {})
-    except RuntimeError:
+        return fetch_git_sha(odoo_dir)
+    except InvalidGitRepository:
         pass
-    else:
-        cxt.update({
-            'session': {
-                'context': session.get('context', {}),
-                'db': session.get('db', None),
-                'login': session.get('login', None),
-                'uid': session.get('uid', None),
-            },
-        })
-    finally:
-        return cxt
 
 
-def serialize_exception(e):
-    '''
-        overrides `openerp.http.serialize_exception`
-        in order to log ORM exceptions.
-    '''
-    if isinstance(e, ORM_EXCEPTIONS):
-        if INCLUDE_USER_CONTEXT:
-            client.extra_context(get_user_context())
-        if ALLOW_ORM_WARNING:
-            client.captureException(sys.exc_info())
-        return openerp.http.serialize_exception(e)
-    elif isinstance(e, Exception):
-        if INCLUDE_USER_CONTEXT:
-            client.extra_context(get_user_context())
-            client.captureException(sys.exc_info())
-    return openerp.http.serialize_exception(e)
+def initialize_raven(config):
+    client_dsn = config.get('sentry_client_dsn', '').strip()
+    enabled = config.get('sentry_enabled', False)
+    report_user_errors = config.get('sentry_report_user_errors', False)
+    include_extra_context = config.get('sentry_include_context', False)
+    level = config.get('sentry_logging_level', DEFAULT_LOG_LEVEL)
+    odoo_dir = config.get('sentry_odoo_dir', None)
+
+    client = Client(
+        client_dsn,
+        install_sys_hook=False,
+        release=get_odoo_commit(odoo_dir),
+    )
+
+    if level not in LOG_LEVEL_MAP:
+        level = DEFAULT_LOG_LEVEL
+
+    if enabled:
+        handler = OdooSentryHandler(
+            include_extra_context,
+            client=client,
+            level=LOG_LEVEL_MAP[level],
+        )
+        if not report_user_errors:
+            handler.addFilter(UserErrorFilter())
+        setup_logging(handler)
+
+    return client
 
 
-class ContextSentryHandler(SentryHandler):
-
-    def __init__(self, allow_orm=False, *args, **kwargs):
-        super(ContextSentryHandler, self).__init__(*args, **kwargs)
-        self.allow_orm = allow_orm
-
-    def emit(self, rec):
-        if not self.allow_orm and isinstance(rec.exc_info, (list, tuple)) and \
-                len(rec.exc_info) >= 2:
-            # Ignore ORM exceptions
-            if isinstance(rec.exc_info[1], ORM_EXCEPTIONS):
-                return
-
-        if INCLUDE_USER_CONTEXT:
-            client.extra_context(get_user_context())
-        super(ContextSentryHandler, self).emit(rec)
-
-
-client = Client(CLIENT_DSN)
-
-if LOGGING_LEVEL not in LOGLEVELS:
-    LOGGING_LEVEL = 'warn'
-
-if ENABLE_LOGGING:
-    # future enhancement: add exclude loggers option
-    EXCLUDE_LOGGER_DEFAULTS += ('werkzeug',)
-    handler = ContextSentryHandler(
-        client=client, level=LOGLEVELS[LOGGING_LEVEL],
-        allow_orm=ALLOW_ORM_WARNING)
-    setup_logging(handler, exclude=EXCLUDE_LOGGER_DEFAULTS)
-
-if ALLOW_ORM_WARNING:
-    openerp.addons.web.controllers.main._serialize_exception = \
-        serialize_exception
-    openerp.addons.report.controllers.main._serialize_exception = \
-        serialize_exception
-
-# wrap the main wsgi app
-openerp.service.wsgi_server.application = Sentry(
-    openerp.service.wsgi_server.application, client=client)
-
-if INCLUDE_USER_CONTEXT:
-    client.extra_context(get_user_context())
-# Fire the first message
+client = initialize_raven(config)
 client.captureMessage('Starting Odoo Server')
